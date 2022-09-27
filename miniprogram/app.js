73b9e39daacfd9fb72fc7dc665d3b7d53589486d
gxh <myoursky723@gmail.com>
2022-09-27 17:10:18 +0800

    first-commit
    Signed-off-by: gxh <myoursky723@gmail.com>

---
+++ miniprogram/app.js
@@ -0,0 +1,13 @@
+App({
+  onLaunch() {
+    // 设置上报渠道信息
+    // tdw.configSource(options);
+    // tdw.config({}, { debug: false });
+  },
+  onError(err) {
+    console.log('App.onError', err);
+  },
+  onPageNotFound(err) {
+    console.log('App.onPageNotFound', err);
+  },
+});
